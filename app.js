ca3869d8281c1a6a9f8a2b840edd60a29e036015
Francisco De La Cruz <helloquico@gmail.com>
2015-05-12 18:52:44 -0400

    Exercise 01: Build a Simple Toolbar - Simple

--- app.js
+++ app.js
@@ -1,28 +1,70 @@
-var createPerson = function(firstname, lastname) {
-	var person = {};
+var oojs = (function(oojs) {
 
-	Object.defineProperties(person, {
-		firstname: {
-			value: firstname,
-			enumerable: true
-		},
-		lastname: {
-			value: lastname,
-			enumerable: true
-		},
-		fullName: {
-			get: function() {
-				return this.firstname + ' ' + this.lastname;
-			},
-			enumerable: true
-		}
-	});
+	var createToolbarItems = function(itemElements) {
+		var items = [];
+
+		[].forEach.call(itemElements, function(el, index, array) {
+
+			var item = {
+				el: el,
+				disable: function() {
+					this.el.classList.add('disabled');
+				},
+				enable: function() {
+					this.el.classList.remove('disabled');
+				},
+				isDisabled: function() {
+					return this.el.classList.contains('disabled');
+				},
+				activate: function() {
+					if (this.isDisabled()) {
+						return;
+					}
+
+					this.el.classList.add('active');
+				},
+				deactivate: function() {
+					if (this.isDisabled()) {
+						return;
+					}
+
+					this.el.classList.remove('active');
+				},
+				isActive: function () {
+					return this.el.classList.contains('active');
+				},
+				toggleActiveState: function() {
+					if (this.isActive()) {
+						this.deactivate();
+					} else {
+						this.activate();
+					}
+				}
+			};
+
+			items.push(item);
+		});
+
+		return items;
+	};
+
+	oojs.createToolbar = function(elementId) {
+		var element = document.getElementById(elementId);
+		var items = element.querySelectorAll('.toolbar-item');
+
+		return {
+			items: createToolbarItems(items)
+		};
+	};
+
+	return oojs;
+}(oojs || {}));
 
 
-	return person;
-};
-
-var person = createPerson('John', 'Doe');
-
-// person.fullName (NO ()) -> 'John Doe'
-// Object.keys(person)     -> ["firstname", "lastname", "fullName"]
+// var toolbar = oojs.createToolbar('myToolbar')
+// toolbar.items[0].disable()
+// toolbar.items[0].isDisabled()
+// toolbar.items[0].enable()
+// toolbar.items[0].activate()
+// toolbar.items[0].deactivate()
+// toolbar.items[0].toggleActiveState()
