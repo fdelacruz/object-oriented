7ec8ed5e4fd3d27aacffb0e3333b5ecd65bd21a7
Francisco De La Cruz <helloquico@gmail.com>
2015-05-12 12:33:51 -0400

    The 'this' Keyword

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-var createPerson = function (firstname, lastname) {
-	return {
-		firstname: firstname,
-		lastname: lastname,
-		sayHi: function () {
-			return 'Hi there';
-		}
-	};
+var makeRequest = function (url, callback) {
+	// make the request with the provided url
+	
+	var data = 10; // coming from the serve in json, text or xml format
+
+	callback(data);
 };
 
-var johnDoe = createPerson("John", "Doe");
+// code that is going to make the request
+var obj = {
+	someValue: 20,
 
-var janeDoe = createPerson("Jane", "Doe");
+	// this is going to be the callback function
+	loadData: function (data) {
 
-var jimDoe = createPerson("Jim", "Doe");
+		// add someValue and the data parameter
+		var sum = this.someValue + data;
 
-// Old way of creating objects
-// var person = new Object();
+		console.log(sum);
+	},
 
-// person.firstname = 'John';
-// person.lastname = 'Doe';
+	// this function calls the makeRequest function
+	prepareRequest: function () {
+		var url = 'http://numberservice.com';
 
-// person.sayHi = function () {
-// 	return 'Hi there';
+		makeRequest(url, this.loadData.bind(this)); // bind loadData() to obj in makeRequest() execution context
+	}
+};
+
+// obj.prepareRequest() -> 30
+
+// // 'this' is dependent upon the object that the function is attached to.
+// window.name = 'Jane Doe';
+
+// var globalGreet = function () {
+// 	return 'My name is ' + this.name;
 // };
+
+// var johnDoe = {
+// 	name: 'John Doe',
+// 	greet: globalGreet
+// };
+
+// johnDoe.greet() -> 'My name is John Doe'
+// globalGreet()   -> 'My name is Jane Doe'
