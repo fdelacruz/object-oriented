7593118b4b6bb6bf8215f5087e6710afd5f3adae
Francisco De La Cruz <helloquico@gmail.com>
2015-05-13 17:36:34 -0400

    First Exercise Homework

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
 var oojs = (function(oojs) {
 
+	var createToolbarItem = function (itemElement) {
+		var item = {
+			toggleActiveState: function() {
+				this.activated = !this.activated;
+			}
+		};
+
+		Object.defineProperties(item, {
+			el: {
+				value: itemElement
+			},
+			enabled: {
+				get: function() {
+					return !this.el.classList.contains('disabled');
+				},
+				set: function(value) {
+					if (value) {
+						this.el.classList.remove('disabled');
+					} else {
+						this.el.classList.add('disabled');
+					}
+				}
+			},
+			activated: {
+				get: function() {
+					return this.el.classList.contains('active');
+				},
+				set: function(value) {
+					if (value) {
+						this.el.classList.add('active');
+					} else {
+						this.el.classList.remove('active');
+					}
+				}
+			}
+		});
+
+		return item;
+	};
+
 	var createToolbarItems = function(itemElements) {
 		var items = [];
 
 		[].forEach.call(itemElements, function(el, index, array) {
-
-			var item = {
-				toggleActiveState: function() {
-					this.activated = !this.activated;
-				}
-			};
-
-			Object.defineProperties(item, {
-				el: {
-					value: el
-				},
-				enabled: {
-					get: function() {
-						return !this.el.classList.contains('disabled');
-					},
-					set: function(value) {
-						if (value) {
-							this.el.classList.remove('disabled');
-						} else {
-							this.el.classList.add('disabled');
-						}
-					}
-				},
-				activated: {
-					get: function() {
-						return this.el.classList.contains('active');
-					},
-					set: function(value) {
-						if (value) {
-							this.el.classList.add('active');
-						} else {
-							this.el.classList.remove('active');
-						}
-					}
-				}
-			});
+			var item = createToolbarItem(el);
 
 			items.push(item);
 		});
@@ -49,11 +54,56 @@ var oojs = (function(oojs) {
 
 	oojs.createToolbar = function(elementId) {
 		var element = document.getElementById(elementId);
+
+		if (!element) {
+			element = document.createElement("DIV");
+			element.id = elementId;
+			element.className = "toolbar";
+		}
+
 		var items = element.querySelectorAll('.toolbar-item');
 
-		return {
-			items: createToolbarItems(items)
+		var toolbar = {
+			add: function (options) {
+				var span = document.createElement("SPAN");
+				span.className = "toolbar-item";
+
+				this.el.appendChild(span);
+
+				var item = createToolbarItem(span);
+
+				this.items.push(item);
+			},
+			remove: function (index) {
+				var len = this.items.length;
+
+				if (index > len || index < 0) {
+					throw new Error("Index is out of range");
+				}
+
+				var item = this.items[index];
+				this.items.splice(index, 1);
+
+				this.el.removeChild(item.el);
+
+				item = null;
+			},
+			appendTo: function (parentElement) {
+				parentElement.appendChild(this.el);
+			}
 		};
+
+		Object.defineProperties(toolbar, {
+			el: {
+				value: element
+			},
+			items: {
+				value: createToolbarItems(items),
+				enumerable : true
+			}
+		});
+
+		return toolbar;
 	};
 
 	return oojs;
@@ -61,8 +111,14 @@ var oojs = (function(oojs) {
 
 
 // var toolbar = oojs.createToolbar('myToolbar')
+// toolbar.add()
+// toolbar.remove(1)
 // toolbar.items[0].enabled = false
 // toolbar.items[0].enabled = true
 // toolbar.items[0].activated = true
 // toolbar.items[0].activated = false
-// toolbar.items[0].toggleActiveState()
+// var toolbar1 = oojs.createToolbar('foo')
+// toolbar1.appendTo(document.body)
+// toolbar1.add()
+// toolbar1.remove(0)
+
